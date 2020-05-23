c68b8697fb52fd55842351dfe0395942dd6e0e1e
Nick Nash <nick.nash.git@gmail.com>
2020-05-22 20:22:14 -0400

    create unused tracked player class
    Signed-off-by: Nick Nash <nick.nash.git@gmail.com>

---
+++ venv/trackedplayers.py
@@ -0,0 +1,20 @@
+# Space for imports
+#
+#
+#
+
+
+class TrackedPlayer:
+
+    def __init__(self, rect_coords):
+        self.coords = rect_coords  # {startx: , starty: , endx: , endy: }
+        self.centroid = []
+        self.calc_centroid()
+        # todo: There is a lot to add here.
+
+    def calc_centroid(self):
+        mid_x = abs(self.coords['startx'] - self.coords['endx'])
+        mid_y = abs(self.coords['starty'] - self.coords['endy'])
+        assert mid_x > 0
+        assert mid_y > 0
+        self.centroid = [mid_x, mid_y]
