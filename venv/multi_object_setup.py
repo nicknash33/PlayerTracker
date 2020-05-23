9e6a1da5fc58f2d5fa714ec88fa33200213cfa5d
Nick Nash <nick.nash.git@gmail.com>
2020-05-23 13:11:54 -0400

    Getting better at not re drawing or tracking objects. Does not dump out of frame objects.
    Signed-off-by: Nick Nash <nick.nash.git@gmail.com>

--- venv/multi_object_setup.py
+++ venv/multi_object_setup.py
@@ -4,6 +4,8 @@ import argparse
 import imutils
 import dlib
 import cv2
+from trackedplayers import TrackedPlayer
+import time
 
 
 class ObjectTracker:
@@ -25,6 +27,9 @@ class ObjectTracker:
         self.current_label = ''
 
         self.detections = None
+        self.fps = ''
+        self.current_detection_object = ''
+        self.last_num_detections = 0
 
         # Coordinate variables.
         self.h = 0
@@ -50,21 +55,25 @@ class ObjectTracker:
         w = self.w
         box = detection_object * np.array([w, h, w, h])
         (self.x0, self.y0, self.x1, self.y1) = box.astype("int")
+        self.calc_center()
 
     def calc_center(self):
-        (startX, startY, endX, endY) = get_detection_box_dims(detection_object)
         mid_x = abs(self.x0 - self.x1)
         mid_y = abs(self.y0 - self.y1)
-        self.centroid = [mid_y, mid_y]
+        self.centroid = [mid_x, mid_y]
 
     def create_tracker(self):
+        p = None  # Clear any reference to player before assignment.
         t = dlib.correlation_tracker()
         rect = dlib.rectangle(self.x0, self.y0, self.x1, self.y1)
         t.start_track(self.rgb, rect)
-        self.labels.append(self.current_label)
-        self.trackers.append(t)
+        self.calc_center()
+        p = TrackedPlayer(self.centroid, t, self.current_label)
+        #self.labels.append(self.current_label)
+        self.trackers.append(p)
 
-    def update_boxes(self, t, label):
+    def update_boxes(self, player):
+        t = player.tracker
         t.update(self.rgb)
         pos = t.get_position()
 
@@ -72,15 +81,41 @@ class ObjectTracker:
         self.y0 = int(pos.top())
         self.x1 = int(pos.right())
         self.y1 = int(pos.bottom())
-
-        self.current_label = label
+        self.calc_center()
+        self.current_label = player.name
+        player.centroid = self.centroid
         self.draw_box()
+        print(f'{self.centroid} {self.current_label}')
 
     def draw_box(self):
         cv2.rectangle(self.frame, (self.x0, self.y0), (self.x1, self.y1),
                       (0, 255, 0), 2)
         cv2.putText(self.frame, self.current_label, (self.x0, self.y0 - 15),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+
+    def clean_up_to_close(self):
+        # stop the timer and display FPS information
+        self.fps.stop()
+        print("[INFO] elapsed time: {:.2f}".format(self.fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(self.fps.fps()))
+
+        # do a bit of cleanup
+        cv2.destroyAllWindows()
+        self.vs.release()
+
+    def is_object_tracked(self):
+        x = self.centroid[0]
+        y = self.centroid[1]
+        print(self.centroid)
+
+        for tracked_player in self.trackers:
+            delta_x = abs(x - tracked_player.centroid[0])
+            delta_y = abs(y - tracked_player.centroid[1])
+            if delta_x < 10 and delta_y < 10:
+                print('Already tracking an object close to here. Skipping')
+                return True
+        else:
+            return False
 
     def start(self):
         # load our serialized model from disk
@@ -92,7 +127,7 @@ class ObjectTracker:
         self.vs = cv2.VideoCapture(self.args['vid'])
 
         # start the frames per second throughput estimator
-        fps = FPS().start()
+        self.fps = FPS().start()
 
         frame_counter = 0
 
@@ -117,20 +152,22 @@ class ObjectTracker:
                     if confidence > self.args["confidence"]:
                         class_label_index = int(self.detections[0, 0, i, 1])
                         detection_label = self.CLASSES[class_label_index]
-                        self.current_label = str(detection_label + str(len(self.labels)))
+                        self.current_label = str(detection_label + str(len(self.trackers)))
 
                         # if the class label is not a person, ignore it
                         if self.current_label[0:6] != "person":
                             continue
 
-                        self.get_detection_box_dims(self.detections[0, 0, i, 3:7])
-                        self.create_tracker()
-
-                        self.draw_box()
+                        self.current_detection_object = self.detections[0, 0, i, 3:7]
+                        self.get_detection_box_dims(self.current_detection_object)
+                        if not self.is_object_tracked():
+                            self.create_tracker()
+                            self.draw_box()
 
             else:
-                for (t, label) in zip(self.trackers, self.labels):
-                    self.update_boxes(t, label)
+                for tracked_player in self.trackers:
+                    self.update_boxes(tracked_player)
+
 
             cv2.imshow("Frame", self.frame)
             key = cv2.waitKey(1) & 0xFF
@@ -138,22 +175,15 @@ class ObjectTracker:
             if key == ord("q"):
                 break
 
-            fps.update()
+            self.fps.update()
 
-        # stop the timer and display FPS information
-        fps.stop()
-        print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-         # do a bit of cleanup
-        cv2.destroyAllWindows()
-        self.vs.release()
+        self.clean_up_to_close()
 
 
 vid = '/Users/nick/PycharmProjects/PlayerTracker/venv/game_film/trimmed_cbj_van.mp4'
 prototxt = 'mobilenet_ssd/MobileNetSSD_deploy.prototxt'
 model = 'mobilenet_ssd/MobileNetSSD_deploy.caffemodel'
-confidence = 0.15
+confidence = 0.10
 
 args_ = {'vid': vid, 'prototxt': prototxt, 'model': model, 'confidence': confidence}
 
--- venv/trackedplayers.py
+++ venv/trackedplayers.py
@@ -6,18 +6,10 @@
 
 class TrackedPlayer:
 
-    def __init__(self, rect_coords, tracker, label):
-        self.coords = rect_coords  # {startx: , starty: , endx: , endy: }
-        self.centroid = []
-        self.calc_centroid()
-        self.t = tracker
+    def __init__(self, center, tracker, label):
+        self.centroid = center
+        self.tracker = tracker
         self.name = label
         self.on_ice = False
         # todo: There is a lot to add here.
 
-    def calc_centroid(self):
-        mid_x = abs(self.coords['startx'] - self.coords['endx'])
-        mid_y = abs(self.coords['starty'] - self.coords['endy'])
-        assert mid_x > 0
-        assert mid_y > 0
-        self.centroid = [mid_x, mid_y]
